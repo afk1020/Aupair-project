e916dd05657d244f13c32a926d129b4ad1524b9c
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-06-02 12:25:53 -0700

    updated login

--- src/components/CreateListing.js
+++ src/components/CreateListing.js
@@ -1,15 +1,57 @@
 import React from 'react';
 import ListingForm from './ListingForm';
 import './Listing.css'
-import { propTypes } from 'react-bootstrap/esm/Image';
 
-const CreateListing = (props) => {
+class CreateListing extends React.Component {
+	state = {
+		image: "",
+		name: "",
+		aupairName: "",
+		salary: "",
+		startDate: ""
+	  };
+	  newValue = (event) => {
+		event.preventDefault();
+		this.props.addListing(this.state)
+		
+	  };
 
+	  handleName = (event) => {
+		this.setState({
+		  name: event.target.value,
+		});
+	  };
+	  handleImage = (event) => {
+		this.setState({
+		  image: event.target.value,
+		});
+	  };
+	
+	  handleAge = (event) =>{
+		this.setState({
+		  aupairName: event.target.value
+		})
+	  }
+	
+	  handleNationality = (event) =>{
+		this.setState({
+		  startDate: event.target.value
+		})
+	  }
+	  
+	  handleCertificate = (event) =>{
+		this.setState({
+		  salary: event.target.value
+		})
+	  }
+	  
+	  render (){
 	return (
 		<div className="App">
-        <ListingForm addListing={props.addListing}/> 
+        <ListingForm addListing={props.addListing}/>
+		<ListingCard /> 
 		</div>
 	)
-
+	}
 }
 export default CreateListing
